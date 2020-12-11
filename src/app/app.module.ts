f827823c9dc630cfa7c204d97df41eb8dac56ce8
Ajit Singh <ajit@poshvine.com>
2020-12-11 10:32:43 +0530

    tailwind class for form field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormFieldComponent } from './form-field/form-field.component';
+import { TwLabelDirective } from './tw-label/tw-label.directive';
+import { TwInputDirective } from './tw-input/tw-input.directive';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormFieldComponent,
+    TwLabelDirective,
+    TwInputDirective,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tw-input/tw-input.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+import { ControlContainer, NgControl } from '@angular/forms';
+
+@Directive({
+  selector: '[twInput]',
+  host: {
+    class: 'block w-full appearance-none focus:outline-none bg-transparent',
+  },
+})
+export class TwInputDirective {
+  @Input('formControlName') formControlName: string;
+  invalid = this.control.invalid;
+  constructor(private control: NgControl) {}
+
+  ngOnInit() {
+    this.invalid = this.control.invalid;
+    this.control.valueChanges.subscribe(() => {
+      this.invalid = this.control.invalid;
+      console.log(this.invalid);
+    });
+  }
+}
